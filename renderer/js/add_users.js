a3558f2363f82137b0dbeda54d290458d3920bbd
DexMP <matveymp@yandex.ru>
2022-06-11 07:23:16 +0300

    last

--- renderer/js/add_users.js
+++ renderer/js/add_users.js
@@ -1,6 +1,5 @@
 'use strict'
 const { ipcRenderer } = require('electron')
-let root
 
 document.getElementById('newUserForm').addEventListener('submit', (evt) => {
     // prevent default refresh functionality of forms
@@ -8,16 +7,10 @@ document.getElementById('newUserForm').addEventListener('submit', (evt) => {
     let username = document.getElementById('username').value
     let fullname = document.getElementById('fullname').value
     let password = document.getElementById('password').value
-    let radioBtn = document.getElementById('root').value
-    if (radioBtn == 'on') {
-        root = true
-        ipcRenderer.send('newUser', [username], [fullname], [password], [root])
-    } else {
-        root = false
-        ipcRenderer.send('newUser', [username], [fullname], [password], [root])
-    }
+    root = false
+    ipcRenderer.send('newUser', [username], [fullname], [password], [root])
     username.value = ''
     fullname.value = ''
     password.value = ''
-    radioBtn.value = ''
+    chekbox.value = ''
 })
